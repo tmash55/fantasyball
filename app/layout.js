4e7df5190518ca70a5f9c6ef4a292b952bd1c10e
TylerMaschoff <66757633+Tymash55@users.noreply.github.com>
2024-06-25 21:04:15 -0500

    layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-
+import PlausibleProvider from "next-plausible";
 import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
@@ -21,7 +21,11 @@ export const metadata = getSEOTags();
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme={config.colors.theme} className={font.className}>
-      {config.domainName && <head></head>}
+      {config.domainName && (
+        <head>
+          <PlausibleProvider domain={config.domainName} />
+        </head>
+      )}
       <body>
         {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
         <ClientLayout>{children}</ClientLayout>
