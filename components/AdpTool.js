4605bbd02afaf5577f7e8a7524a90440fa5e9608
TylerMaschoff <66757633+Tymash55@users.noreply.github.com>
2024-08-22 16:01:06 -0500

    Fix Svg

--- components/AdpTool.js
+++ components/AdpTool.js
@@ -430,10 +430,8 @@ const AdpTool = () => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="icon icon-tabler icons-tabler-outline icon-tabler-download"
+              strokeWidth="2"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-download"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
